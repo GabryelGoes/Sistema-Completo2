1e13cb4dd4b2cf5a6dfcab26ee9fd87ff8686d02
GabryelGoes <goesgabryel@gmail.com>
2026-05-10 19:05:07 -0300

    Update workshop_id in realtime select policies

--- supabase/migrations/20260511120000_realtime_anon_select_policies.sql
+++ supabase/migrations/20260511120000_realtime_anon_select_policies.sql
@@ -15,17 +15,17 @@ ALTER TABLE public.workshop_reminders ENABLE ROW LEVEL SECURITY;
 DROP POLICY IF EXISTS realtime_anon_select_service_orders ON public.service_orders;
 CREATE POLICY realtime_anon_select_service_orders ON public.service_orders
   FOR SELECT TO anon
-  USING (workshop_id = '00000000-0000-0000-0000-000000000001'::uuid);
+  USING (workshop_id = '624ccf66-a342-4769-9d09-053cd1d565b6'::uuid);
 
 DROP POLICY IF EXISTS realtime_anon_select_budgets ON public.budgets;
 CREATE POLICY realtime_anon_select_budgets ON public.budgets
   FOR SELECT TO anon
-  USING (workshop_id = '00000000-0000-0000-0000-000000000001'::uuid);
+  USING (workshop_id = '624ccf66-a342-4769-9d09-053cd1d565b6'::uuid);
 
 DROP POLICY IF EXISTS realtime_anon_select_workshop_reminders ON public.workshop_reminders;
 CREATE POLICY realtime_anon_select_workshop_reminders ON public.workshop_reminders
   FOR SELECT TO anon
-  USING (workshop_id = '00000000-0000-0000-0000-000000000001'::uuid);
+  USING (workshop_id = '624ccf66-a342-4769-9d09-053cd1d565b6'::uuid);
 
 DROP POLICY IF EXISTS realtime_anon_select_comments ON public.service_order_comments;
 CREATE POLICY realtime_anon_select_comments ON public.service_order_comments
@@ -35,7 +35,7 @@ CREATE POLICY realtime_anon_select_comments ON public.service_order_comments
       SELECT 1
       FROM public.service_orders so
       WHERE so.id = service_order_comments.service_order_id
-        AND so.workshop_id = '00000000-0000-0000-0000-000000000001'::uuid
+        AND so.workshop_id = '624ccf66-a342-4769-9d09-053cd1d565b6'::uuid
     )
   );
 
@@ -47,7 +47,7 @@ CREATE POLICY realtime_anon_select_checklist_checks ON public.service_order_chec
       SELECT 1
       FROM public.service_orders so
       WHERE so.id = service_order_checklist_checks.service_order_id
-        AND so.workshop_id = '00000000-0000-0000-0000-000000000001'::uuid
+        AND so.workshop_id = '624ccf66-a342-4769-9d09-053cd1d565b6'::uuid
     )
   );
 
@@ -59,6 +59,6 @@ CREATE POLICY realtime_anon_select_customers ON public.customers
       SELECT 1
       FROM public.service_orders so
       WHERE so.customer_id = customers.id
-        AND so.workshop_id = '00000000-0000-0000-0000-000000000001'::uuid
+        AND so.workshop_id = '624ccf66-a342-4769-9d09-053cd1d565b6'::uuid
     )
   );
